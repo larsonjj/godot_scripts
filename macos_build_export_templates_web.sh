75065da8b65e392776b74b7b7b2b98c4ac85e54b
Jake Larson <jake.j.larson@gmail.com>
2026-02-27 11:35:55 -0600

    Update export templates and profiles for Godot version 4.6.1; refactor disabled classes and features

--- macos_build_export_templates_web.sh
+++ macos_build_export_templates_web.sh
@@ -7,7 +7,7 @@ CURRENT_DIR=$(pwd)
 CUSTOM_PROFILE="$CURRENT_DIR/custom_2d.py"
 CUSTOM_BUILD_PROFILE="$CURRENT_DIR/custom_2d_template.build"
 GODOT_SOURCE_DIR="$HOME/godot"
-GODOT_VERSION="4.6.stable"
+GODOT_VERSION="4.6.1.stable"
 GODOT_APP_DIR="$HOME/Library/Application Support/Godot"
 OUTPUT_DIR="$GODOT_APP_DIR/export_templates/$GODOT_VERSION"
 
@@ -15,16 +15,16 @@ OUTPUT_DIR="$GODOT_APP_DIR/export_templates/$GODOT_VERSION"
 cd $GODOT_SOURCE_DIR
 
 # Release
-scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_release tools=no production=yes javascript_eval=no use_volk=no vulkan=no debug_symbols=no optimize=size lto=full threads=no linkflags="-Wl,-u,htonl"
-scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_release dlink_enabled=yes tools=no production=yes javascript_eval=no use_volk=no vulkan=no debug_symbols=no optimize=size lto=full threads=no linkflags="-Wl,-u,htonl"
-scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_release tools=no production=yes javascript_eval=no use_volk=no vulkan=no debug_symbols=no optimize=size lto=full threads=yes linkflags="-Wl,-u,htonl"
-scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_release dlink_enabled=yes tools=no production=yes javascript_eval=no use_volk=no vulkan=no debug_symbols=no optimize=size lto=full threads=yes linkflags="-Wl,-u,htonl"
+scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_release tools=no production=yes javascript_eval=no debug_symbols=no optimize=size lto=full threads=no linkflags="-Wl,-u,htonl"
+scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_release dlink_enabled=yes tools=no production=yes javascript_eval=no debug_symbols=no optimize=size lto=full threads=no linkflags="-Wl,-u,htonl"
+scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_release tools=no production=yes javascript_eval=no debug_symbols=no optimize=size lto=full threads=yes linkflags="-Wl,-u,htonl"
+scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_release dlink_enabled=yes tools=no production=yes javascript_eval=no debug_symbols=no optimize=size lto=full threads=yes linkflags="-Wl,-u,htonl"
 
 # Debug
-scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_debug tools=no production=yes javascript_eval=no use_volk=no vulkan=no optimize=size lto=none threads=no linkflags="-Wl,-u,htonl"
-scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_debug dlink_enabled=yes tools=no production=yes javascript_eval=no use_volk=no vulkan=no optimize=size lto=none threads=no linkflags="-Wl,-u,htonl"
-scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_debug tools=no production=yes javascript_eval=no use_volk=no vulkan=no optimize=size lto=none threads=yes linkflags="-Wl,-u,htonl"
-scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_debug dlink_enabled=yes tools=no production=yes javascript_eval=no use_volk=no vulkan=no optimize=size lto=none threads=yes linkflags="-Wl,-u,htonl"
+scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_debug tools=no production=yes javascript_eval=no optimize=size lto=none threads=no linkflags="-Wl,-u,htonl"
+scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_debug dlink_enabled=yes tools=no production=yes javascript_eval=no optimize=size lto=none threads=no linkflags="-Wl,-u,htonl"
+scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_debug tools=no production=yes javascript_eval=no optimize=size lto=none threads=yes linkflags="-Wl,-u,htonl"
+scons profile=$CUSTOM_PROFILE build_profile=$CUSTOM_BUILD_PROFILE platform=web target=template_debug dlink_enabled=yes tools=no production=yes javascript_eval=no optimize=size lto=none threads=yes linkflags="-Wl,-u,htonl"
 
 # Move to export template folder
 mkdir -p "$OUTPUT_DIR"
