02af3286a394dab4376ecdf702b72d94ee0d5cb7
Jake Larson <jake.j.larson@gmail.com>
2026-02-27 12:42:00 -0600

    Refactor custom 2D script to include additional size-saving options for various modules

--- custom_2d.py
+++ custom_2d.py
@@ -8,3 +8,43 @@ module_openxr_enabled = "no"
 minizip = "no"
 module_text_server_adv_enabled = "no"
 module_text_server_fb_enabled = "yes"
+
+# Additional size savings - uncomment based on your needs:
+
+# Scripting (only disable if not using these languages)
+# module_mono_enabled = "no"          # C# support (~large savings)
+
+# Audio
+# module_ogg_enabled = "no"           # OGG audio format
+# module_vorbis_enabled = "no"        # Vorbis audio codec (pairs with OGG)
+# module_opus_enabled = "no"          # Opus audio codec
+# module_minimp3_enabled = "no"       # MP3 audio format
+
+# Video
+# module_theora_enabled = "no"        # OGG Theora video playback
+# module_webm_enabled = "no"          # WebM video playback
+
+# Image formats
+# module_basis_universal_enabled = "no"  # Basis Universal texture compression (3D focused)
+# module_bmp_enabled = "no"              # BMP image support
+module_tga_enabled = "no"  # TGA image support
+# module_svg_enabled = "no"              # SVG image support
+module_hdr_enabled = "no"  # HDR image support (mostly 3D)
+module_dds_enabled = "no"  # DDS texture support (mostly 3D)
+module_ktx_enabled = "no"  # KTX texture support (mostly 3D)
+
+# Networking (disable if no multiplayer/networking)
+# module_enet_enabled = "no"             # ENet UDP networking
+# module_websocket_enabled = "no"        # WebSocket support
+# module_webrtc_enabled = "no"           # WebRTC support
+# module_multiplayer_enabled = "no"      # High-level multiplayer API
+
+# Physics (2D only, disable 3D alternatives)
+module_godot_physics_3d_enabled = "no"  # Godot's built-in 3D physics
+
+# Misc
+module_camera_enabled = "no"  # Camera (device webcam) access
+# module_jsonrpc_enabled = "no"          # JSON-RPC support
+# module_mbedtls_enabled = "no"          # TLS/SSL (disables HTTPS/WSS)
+# module_noise_enabled = "no"            # FastNoiseLite (procedural noise)
+# module_navigation_enabled = "no"       # 2D navigation mesh (if not using NavigationAgent2D)
